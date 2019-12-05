437a4e848d33cf8402f6ec1353a54b7b39e16adf
0624040 <55527010+0624040@users.noreply.github.com>
2019-12-05 15:18:39 +0800

    Add files via upload

--- Homework3.java
+++ Homework3.java
@@ -3,3 +3,10 @@ Course[] courses = new Course[10];
 }
 class Course {
 }
+
+public class Homework3{
+    public static void main(String args[]){
+        Student student=new Student();
+        Course courses=new Course();
+    }
+}
